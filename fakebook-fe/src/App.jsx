9639ec49930145e65b50db572f3a9cacc2e73cbb
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-18 18:12:40 +0200

    Adding <Login/> page to the App.jsx, including its styles.

--- fakebook-fe/src/App.jsx
+++ fakebook-fe/src/App.jsx
@@ -1,7 +1,14 @@
-import './App.scss'
+import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
 
 function App() {
-
+  return (
+    <div className="App">
+      <Login />
+      <Footer/>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- fakebook-fe/src/pages/Login/Login.jsx
+++ fakebook-fe/src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import './Login.scss';
+import "./Login.scss";
 
 const Login = () => {
     return ( 
