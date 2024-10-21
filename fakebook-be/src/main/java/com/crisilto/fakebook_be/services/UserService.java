c05cc38177f7e387751383637f4629a3088c9585
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-21 21:22:45 +0200

    Add loadUserByUsername method. Class UserService implements UserDetailsService.

--- fakebook-be/src/main/java/com/crisilto/fakebook_be/services/UserService.java
+++ fakebook-be/src/main/java/com/crisilto/fakebook_be/services/UserService.java
@@ -3,11 +3,16 @@ package com.crisilto.fakebook_be.services;
 import com.crisilto.fakebook_be.models.User;
 import com.crisilto.fakebook_be.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -19,6 +24,15 @@ public class UserService {
         User user = userRepository.findByEmail(email);
 
         return user != null && passwordEncoder.matches(rawPassword, user.getPassword());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 
     public User findByEmail(String email) {
