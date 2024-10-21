feb8cca5b255b89ee71a39759aa09a7300f78b45
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-21 21:24:47 +0200

    Improve @Post /login - Add logic to generate a JWT token when user is authenticated correctly and return it as response.

--- fakebook-be/src/main/java/com/crisilto/fakebook_be/controllers/AuthController.java
+++ fakebook-be/src/main/java/com/crisilto/fakebook_be/controllers/AuthController.java
@@ -1,10 +1,12 @@
 package com.crisilto.fakebook_be.controllers;
 
 import com.crisilto.fakebook_be.requests.LoginRequest;
+import com.crisilto.fakebook_be.security.JwUtil;
 import com.crisilto.fakebook_be.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +19,19 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private JwUtil jwUtil;
+
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         boolean isAuthenticated = userService.authenticateUser(loginRequest.getEmail(), loginRequest.getPassword());
 
-        if(isAuthenticated){
-            return ResponseEntity.ok("Logged in successfully");
-        }else{
+        if (isAuthenticated) {
+            UserDetails userDetails = userService.loadUserByUsername(loginRequest.getEmail());
+            String jwt = jwUtil.generateToken(userDetails);
+
+            return ResponseEntity.ok(new AuthenticationResponse(jwt));
+        } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
         }
     }
