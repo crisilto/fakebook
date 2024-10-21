47dd23bffeaa1506858977182ea6406f92e2d386
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-21 21:30:02 +0200

    Create SecurityConfiguration - This file will define the security for the application.

---
+++ fakebook-be/src/main/java/com/crisilto/fakebook_be/security/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.crisilto.fakebook_be.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+    // TODO: Implement security configuration here
+}
