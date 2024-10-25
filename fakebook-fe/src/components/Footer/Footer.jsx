0b242a9ca5a71596361dfd59aab74178bf166be8
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-25 11:25:08 +0200

    Last styles fix for Login page

--- fakebook-fe/src/components/Footer/Footer.jsx
+++ fakebook-fe/src/components/Footer/Footer.jsx
@@ -4,48 +4,50 @@ import "./Footer.scss";
 const Footer = ({ className }) => {
   return (
     <footer className={`footer-container ${className}`}>
-      <div className="languages">
-        <ul>
-          <li>
-            <a href="#">English (US)</a>
-          </li>
-          <li>
-            <a href="#">Español</a>
-          </li>
-          <li>
-            <a href="#">Français</a>
-          </li>
-          <li>
-            <a href="#">Deutsch</a>
-          </li>
-        </ul>
-      </div>
-      <div className="link-list">
-        <ul>
-          <li>
-            <a href="#">Sign Up</a>
-          </li>
-          <li>
-            <a href="#">Log In</a>
-          </li>
-          <li>
-            <a href="#">Messenger</a>
-          </li>
-          <li>
-            <a href="#">Privacy Policy</a>
-          </li>
-          <li>
-            <a href="#">Terms</a>
-          </li>
-          <li>
-            <a href="#">Help</a>
-          </li>
-        </ul>
-      </div>
-      <div className="footer-copyright">
-        <p>
-          <a href="#">Cristina Silvestre</a>&copy; 2024{" "}
-        </p>
+      <div className="footer-content">
+        <div className="languages">
+          <ul>
+            <li>
+              <a href="#">English (US)</a>
+            </li>
+            <li>
+              <a href="#">Español</a>
+            </li>
+            <li>
+              <a href="#">Français</a>
+            </li>
+            <li>
+              <a href="#">Deutsch</a>
+            </li>
+          </ul>
+        </div>
+        <div className="link-list">
+          <ul>
+            <li>
+              <a href="#">Sign Up</a>
+            </li>
+            <li>
+              <a href="#">Log In</a>
+            </li>
+            <li>
+              <a href="#">Messenger</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#">Terms</a>
+            </li>
+            <li>
+              <a href="#">Help</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-copyright">
+          <p>
+            <a href="#">Cristina Silvestre</a>&copy; 2024{" "}
+          </p>
+        </div>
       </div>
     </footer>
   );
