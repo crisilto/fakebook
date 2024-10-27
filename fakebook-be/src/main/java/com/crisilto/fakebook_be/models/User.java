825373dd73b20c72b41f5cdff1eddc56cc2e7e36
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-27 20:20:46 +0100

    Rename mobileOrEmail -> contactInfo

--- fakebook-be/src/main/java/com/crisilto/fakebook_be/models/User.java
+++ fakebook-be/src/main/java/com/crisilto/fakebook_be/models/User.java
@@ -13,8 +13,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Generates a unique ID for each user automatically
     private Long id;
 
-    @Column(nullable = false, unique = true) //Ensures mobileOrEmail is unique and not null
-    private String mobileOrEmail;
+    @Column(nullable = false, unique = true) //Ensures getContactInfo is unique and not null
+    private String contactInfo;
 
     @Column(nullable = false) //Ensures password is not null
     private String firstName, lastName, gender, birthday, password;
@@ -26,19 +26,19 @@ public class User {
     public User() {}
 
     //Complete constructor to facilitate user login
-    public User(String mobileOrEmail, String password) {
-        this.mobileOrEmail = mobileOrEmail;
+    public User(String contactInfo, String password) {
+        this.contactInfo = contactInfo;
         this.password = password;
     }
 
     //Complete constructor to facilitate user registration
-    public User(String firstName, String lastName, String birthday, String gender,  String pronoun, String mobileOrEmail, String password) {
+    public User(String firstName, String lastName, String birthday, String gender,  String pronoun, String contactInfo, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthday = birthday;
         this.gender = gender;
         this.pronoun = pronoun;
-        this.mobileOrEmail = mobileOrEmail;
+        this.contactInfo = contactInfo;
         this.password = password;
     }
 }
