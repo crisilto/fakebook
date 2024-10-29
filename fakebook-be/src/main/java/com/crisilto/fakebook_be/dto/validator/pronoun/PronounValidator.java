cb919686048b3d4cc576ead13aa7a60bd13d4b8a
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-29 13:49:11 +0100

    Improve - Require pronoun selection when gender is custom

--- fakebook-be/src/main/java/com/crisilto/fakebook_be/dto/validator/pronoun/PronounValidator.java
+++ fakebook-be/src/main/java/com/crisilto/fakebook_be/dto/validator/pronoun/PronounValidator.java
@@ -1,15 +1,20 @@
 package com.crisilto.fakebook_be.dto.validator.pronoun;
 
-import com.crisilto.fakebook_be.dto.RegisterRequest;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+
+import com.crisilto.fakebook_be.dto.RegisterRequest;
 
 public class PronounValidator implements ConstraintValidator<ValidPronoun, RegisterRequest> {
     @Override
     public boolean isValid(RegisterRequest request, ConstraintValidatorContext context) {
+        
+        if(request.getGender() == null || request.getGender().isEmpty()){
+            return false;
+        }
+
         if("custom".equalsIgnoreCase(request.getGender())){
-            return request.getPronoun() != null && request.getPronoun().isEmpty();
+            return request.getPronoun() != null && !request.getPronoun().isEmpty();
         }
         return true;
     }
