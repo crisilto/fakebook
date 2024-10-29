1af4fa1e0c966b28efc5575d054c7239753ef90f
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-29 14:20:49 +0100

    Update utility function that generates te years - Provides a more appropiate range

--- fakebook-fe/src/utils/dateUtils.js
+++ fakebook-fe/src/utils/dateUtils.js
@@ -1,11 +1,22 @@
-export const getMonths = () => {
-    return ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-};
-
-export const getDays = () => {
-    return Array.from({ length: 31 }, (_, i) => i + 1);
-};
-
 export const getYears = () => {
-    return Array.from({ length: 120 }, (_, i) => new Date().getFullYear() - i);
+    const currentYear = new Date().getFullYear();
+    const minAge = 10;
+    const maxAge = 100;
+    const years = [];
+
+    for (let year = currentYear - minAge; year >= currentYear - maxAge; year--) {
+        years.push(year);
+    }
+
+    return years;
+};
+
+export const getMonths = () => [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+export const getDays = (month) => {
+    const daysInMonth = new Date(new Date().getFullYear(), month, 0).getDate();
+    return Array.from({ length: daysInMonth }, (_, i) => i + 1);
 };
