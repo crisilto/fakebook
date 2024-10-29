f983fe28015b5bf6aeaf80e164b47b108a2ae30d
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-29 14:20:14 +0100

    Update - Ensure consistent age validation

--- fakebook-be/src/main/java/com/crisilto/fakebook_be/dto/validator/birthday/BirthdayValidator.java
+++ fakebook-be/src/main/java/com/crisilto/fakebook_be/dto/validator/birthday/BirthdayValidator.java
@@ -1,25 +1,29 @@
 package com.crisilto.fakebook_be.dto.validator.birthday;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
 public class BirthdayValidator implements ConstraintValidator<ValidBirthday, String> {
     @Override
-    public void initialize(ValidBirthday constraintAnnotation) {
-    }
-
-    @Override
     public boolean isValid(String birthday, ConstraintValidatorContext context) {
-        try{
+        try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             LocalDate birthDate = LocalDate.parse(birthday, formatter);
             LocalDate today = LocalDate.now();
 
             int age = today.getYear() - birthDate.getYear();
-            return age  >= 10 && age <= 100;
-        }catch (Exception e) {
+            
+            if (today.getMonthValue() < birthDate.getMonthValue() || 
+                (today.getMonthValue() == birthDate.getMonthValue() && 
+                 today.getDayOfMonth() < birthDate.getDayOfMonth())) {
+                age--;
+            }
+
+            return age >= 10 && age <= 100;
+        } catch (Exception e) {
             return false;
         }
     }
