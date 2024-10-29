18d3844c87c6f83e8677f9499484db3f1df1088c
Crisilto <cristinasilvestre1708@gmail.com>
2024-10-29 13:41:17 +0100

    Update - Format properly and pass the date

--- fakebook-fe/src/pages/Register/BirthdaySelect/BirthdaySelect.jsx
+++ fakebook-fe/src/pages/Register/BirthdaySelect/BirthdaySelect.jsx
@@ -1,10 +1,38 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
 import { getDays, getMonths, getYears } from "../../../utils/dateUtils";
 
-const BirthdaySelect = () => {
+const BirthdaySelect = ({onSelect}) => {
   const [selectedMonth, setSelectedMonth] = useState("");
   const [selectedDay, setSelectedDay] = useState("");
   const [selectedYear, setSelectedYear] = useState("");
+
+  const updateBirthday = (month, day, year) => {
+    if (month && day && year) {
+      const formattedMonth = String(month).padStart(2, '0');
+      const formattedDay = String(day).padStart(2, '0');
+      const dateString = `${year}-${formattedMonth}-${formattedDay}`;
+      onSelect(dateString);
+    }
+  };
+
+  const handleMonthChange = (e) => {
+    const newMonth = e.target.value;
+    setSelectedMonth(newMonth);
+    updateBirthday(newMonth, selectedDay, selectedYear);
+  };
+
+  const handleDayChange = (e) => {
+    const newDay = e.target.value;
+    setSelectedDay(newDay);
+    updateBirthday(selectedMonth, newDay, selectedYear);
+  };
+
+  const handleYearChange = (e) => {
+    const newYear = e.target.value;
+    setSelectedYear(newYear);
+    updateBirthday(selectedMonth, selectedDay, newYear);
+  };
 
   return (
     <div className="form-group">
@@ -12,7 +40,7 @@ const BirthdaySelect = () => {
       <div className="birthday-select">
         <select
           value={selectedMonth}
-          onChange={(e) => setSelectedMonth(e.target.value)}
+          onChange={handleMonthChange}
         >
           {getMonths().map((month, index) => (
             <option key={index} value={month}>
@@ -23,7 +51,7 @@ const BirthdaySelect = () => {
 
         <select
           value={selectedDay}
-          onChange={(e) => setSelectedDay(e.target.value)}
+          onChange={handleDayChange}
         >
           {getDays(selectedMonth).map((day, index) => (
             <option key={index} value={day}>
@@ -34,7 +62,7 @@ const BirthdaySelect = () => {
 
         <select
           value={selectedYear}
-          onChange={(e) => setSelectedYear(e.target.value)}
+          onChange={handleYearChange}
         >
           {getYears().map((year, index) => (
             <option key={index} value={year}>
@@ -48,3 +76,7 @@ const BirthdaySelect = () => {
 };
 
 export default BirthdaySelect;
+
+BirthdaySelect.propTypes = {
+  onSelect: PropTypes.func.isRequired,
+};
