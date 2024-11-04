146fadb899def79a37ac81deb56443eaf30641a2
Crisilto <cristinasilvestre1708@gmail.com>
2024-11-04 19:21:37 +0100

    - Include Route element of /home

--- fakebook-fe/src/App.tsx
+++ fakebook-fe/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.scss";
+import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 
@@ -11,6 +12,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
