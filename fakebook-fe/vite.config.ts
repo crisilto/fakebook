64b0ecc95506dc41667b5fb09e234aab13f7ced0
Crisilto <cristinasilvestre1708@gmail.com>
2024-11-04 15:48:30 +0100

    - Added SCSS preprocessor options in Vite configuration.

--- fakebook-fe/vite.config.ts
+++ fakebook-fe/vite.config.ts
@@ -5,5 +5,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        charset: false,
+        additionalData: `@use "sass:math";`
+      },
+    },
+  },
 });
