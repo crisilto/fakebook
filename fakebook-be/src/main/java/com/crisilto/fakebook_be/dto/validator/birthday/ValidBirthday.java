05ee45a25f915d4419aa4e7b1d3ea33a59505472
Crisilto <cristinasilvestre1708@gmail.com>
2024-11-04 15:46:09 +0100

    - Enhanced ValidBirthday annotation by using a constant from the ErrorMessages class for error messages.

--- fakebook-be/src/main/java/com/crisilto/fakebook_be/dto/validator/birthday/ValidBirthday.java
+++ fakebook-be/src/main/java/com/crisilto/fakebook_be/dto/validator/birthday/ValidBirthday.java
@@ -1,17 +1,20 @@
 package com.crisilto.fakebook_be.dto.validator.birthday;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import com.crisilto.fakebook_be.utils.ErrorMessages;
+
 @Constraint(validatedBy = BirthdayValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidBirthday {
-    String message() default "It looks like you entered the wrong info. Please be sure to use your real birthday.";
+    String message() default ErrorMessages.AGE_VALIDATION;
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
